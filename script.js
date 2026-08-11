e5a6777ddb2cc180843ed6734974a676ae5218f2
Utkarsh Yadav <utkarshyadav3436@gmail.com>
2026-08-11 22:37:13 +0530

    Start Javascript basics

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello,I'm Utkarsh");
+console.log("I am learning Javascript");
+let name = "Utkarsh";
+let age = 19;
+let course = "B.Tech CSE";
+console.log(name);
+console.log(age);
+console.log(course);
+const github = "https://github.com/utkarshyadav3436-afk";
+
+console.log(github);
